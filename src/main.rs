eff032c9e2d0ae3bc7f61463e8d53e549c67f303
Adam Guyot <adam@cobenian.com>
2025-06-13 00:26:46 -0400

    added null and list

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 enum Value {
@@ -39,14 +39,18 @@ enum OpCode {
     Not,
     And,
     Or,
+    Null,
+    MakeList(usize), // operand: how many items to pop
+    Len,
+    Index,
     DumpScope,
 }
 
 struct VM {
     stack: Vec<Value>,
     instructions: Vec<OpCode>,
-    ip: usize, // instruction pointer
-    call_stack: Vec<usize>, // return addresses for CALL/RET
+    ip: usize,                              // instruction pointer
+    call_stack: Vec<usize>,                 // return addresses for CALL/RET
     variables: Vec<HashMap<String, Value>>, // call frame stack
 }
 
@@ -60,7 +64,6 @@ impl VM {
             variables: vec![HashMap::new()], // global frame
         }
     }
-    
 
     fn run(&mut self) {
         while self.ip < self.instructions.len() {
@@ -96,13 +99,14 @@ impl VM {
                     let is_zero = match val {
                         Value::Int(0) => true,
                         Value::Bool(false) => true,
+                        Value::Null => true,
                         _ => false,
                     };
                     if is_zero {
                         self.ip = *target;
                         continue;
                     }
-                }                
+                }
                 OpCode::Call(target) => {
                     self.call_stack.push(self.ip + 1);
                     self.variables.push(HashMap::new()); // push new scope
@@ -125,7 +129,7 @@ impl VM {
                 OpCode::Dup => {
                     let val = self.stack.last().expect("stack underflow on DUP").clone();
                     self.stack.push(val);
-                }      
+                }
                 OpCode::Store(name) => {
                     let val = self.stack.pop().expect("stack underflow on STORE");
                     self.variables
@@ -134,7 +138,8 @@ impl VM {
                         .insert(name.clone(), val);
                 }
                 OpCode::Load(name) => {
-                    let val = self.variables
+                    let val = self
+                        .variables
                         .last()
                         .expect("no variable scope")
                         .get(name)
@@ -143,7 +148,8 @@ impl VM {
                     self.stack.push(val);
                 }
                 OpCode::Delete(name) => {
-                    let removed = self.variables
+                    let removed = self
+                        .variables
                         .last_mut()
                         .expect("no variable scope")
                         .remove(name);
@@ -160,7 +166,7 @@ impl VM {
                         _ => panic!("EQ requires values of the same type"),
                     };
                     self.stack.push(Value::Int(if result { 1 } else { 0 }));
-                } 
+                }
                 OpCode::Gt => {
                     let b = self.stack.pop().expect("stack underflow on GT");
                     let a = self.stack.pop().expect("stack underflow on GT");
@@ -180,7 +186,7 @@ impl VM {
                         }
                         _ => panic!("LT requires two integers"),
                     }
-                }      
+                }
                 OpCode::Ne => {
                     let b = self.stack.pop().expect("stack underflow on NE");
                     let a = self.stack.pop().expect("stack underflow on NE");
@@ -190,7 +196,7 @@ impl VM {
                         _ => panic!("NE requires values of the same type"),
                     };
                     self.stack.push(Value::Int(if result { 1 } else { 0 }));
-                }   
+                }
                 OpCode::Ge => {
                     let b = self.stack.pop().expect("stack underflow on GE");
                     let a = self.stack.pop().expect("stack underflow on GE");
@@ -240,13 +246,48 @@ impl VM {
                     };
                     self.stack.push(Value::Bool(result));
                 }
+                OpCode::Null => {
+                    self.stack.push(Value::Null);
+                }
+                OpCode::MakeList(n) => {
+                    if self.stack.len() < *n {
+                        panic!("Not enough values on stack for MAKE_LIST");
+                    }
+                    let mut list = Vec::with_capacity(*n);
+                    for _ in 0..*n {
+                        list.push(self.stack.pop().unwrap());
+                    }
+                    list.reverse(); // keep original order
+                    self.stack.push(Value::List(list));
+                }
+                OpCode::Len => {
+                    let val = self.stack.pop().expect("stack underflow on LEN");
+                    match val {
+                        Value::List(l) => self.stack.push(Value::Int(l.len() as i64)),
+                        _ => panic!("LEN expects a list"),
+                    }
+                }
+                OpCode::Index => {
+                    let index = match self.stack.pop().expect("stack underflow on INDEX") {
+                        Value::Int(i) => i as usize,
+                        _ => panic!("INDEX expects an integer index"),
+                    };
+                    let list = match self.stack.pop().expect("stack underflow on INDEX") {
+                        Value::List(l) => l,
+                        _ => panic!("INDEX expects a list"),
+                    };
+                    if index >= list.len() {
+                        panic!("Index out of bounds");
+                    }
+                    self.stack.push(list[index].clone());
+                }
                 OpCode::DumpScope => {
                     println!("Current scope: {:?}", self.variables.last());
                 }
                 OpCode::Halt => break,
             }
             self.ip += 1;
-        }        
+        }
     }
 }
 
@@ -328,6 +369,13 @@ fn parse_program(path: &str) -> Vec<OpCode> {
             "NOT" => OpCode::Not,
             "AND" => OpCode::And,
             "OR" => OpCode::Or,
+            "NULL" => OpCode::Null,
+            "MAKE_LIST" => {
+                let n = parts[1].parse::<usize>().expect("Invalid list size");
+                OpCode::MakeList(n)
+            }
+            "LEN" => OpCode::Len,
+            "INDEX" => OpCode::Index,
             "DUMP_SCOPE" => OpCode::DumpScope,
             _ => panic!("Unknown instruction: {line} on line {line_num}"),
         };
